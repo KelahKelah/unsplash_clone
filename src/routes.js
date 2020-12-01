b317ab0f246309e1d14f2794aef04edf0ccb8dee
KelahKelah <kelechiekebor@gmail.com>
2020-12-01 11:30:59 +0100

    add routes for the base urls

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import SinglePhoto from './components/singlePhoto/singlePhoto';
 import Home from './pages/home/home';
 
 const Routes = () => {
@@ -8,6 +9,7 @@ const Routes = () => {
             <BrowserRouter>
             <Switch>
                 <Route path="/" component={Home} />
+                <Route path="/single/photo" component={SinglePhoto} />
             </Switch>
             </BrowserRouter>
         </div>
