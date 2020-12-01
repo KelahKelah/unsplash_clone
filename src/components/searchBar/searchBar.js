43324f35b3f4e39b32ec80954f0906cc309824da
KelahKelah <kelechiekebor@gmail.com>
2020-12-01 11:33:59 +0100

    design search component

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import  Axios from 'axios';
+import './searchBar.css';
+import {BsSearch} from 'react-icons/bs'
 
 class SearchBar extends Component {
     constructor(props) {
@@ -9,7 +11,10 @@ class SearchBar extends Component {
     render() {
         return(
             <div>
-                
+                <form>
+                    <BsSearch />
+                    <input type="text" className="searchBar" placeholder="Search for photo" />
+                </form>
             </div>
         )
     }
